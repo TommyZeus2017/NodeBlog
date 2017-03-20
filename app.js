2b7396430790de99665e0c540103edd62eeba995
Tommy2017 <sacredzeus@outlook.com>
2017-03-20 15:30:32 +0800

    Dybnamic web - route $ ejs

--- app.js
+++ app.js
@@ -2,8 +2,31 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-// app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, '/public')));
+
+var data = { count: 0 };
+
+app.get('/', function (req, res) {
+    data.count++;
+    res.render('my_first_ejs', data);
+});
+
+app.get('/reset', function (req, res) {
+    data.count = 0;
+    res.render('my_first_ejs', data);
+});
+
+app.get('/set', function (req, res) {
+    if (req.query.count)
+        data.count = req.query.count;
+    res.render('my_first_ejs', data);
+});
+
+app.get('/set/:number', function (req, res) {
+    data.count = req.params.number;
+    res.render('my_first_ejs', data);
+});
 
 app.listen(3000, function() {
     console.log(__dirname);
