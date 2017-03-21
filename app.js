9b442f9f92bd095ffd600855dee97858c0bc33c9
Tommy2017 <sacredzeus@outlook.com>
2017-03-21 18:02:15 +0800

    Account Login

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // import modules
-var express = require('express');
-var app = express();
-var path = require('path');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+var express         = require('express');
+var app             = express();
+var path            = require('path');
+var mongoose        = require('mongoose');
+var passport        = require('passport');
+var session         = require('express-session');
+var flash           = require('connect-flash');
+var sync            = require('async');
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
 
 //===============================================================
 // reference : mongoosejs.com/docs/api.html
@@ -33,30 +38,232 @@ var postSchema = mongoose.Schema({
 
 var Post = mongoose.model('post', postSchema);
 
+var userSchema = mongoose.Schema({
+    email: {type: String, required: true, unique:true},
+    nickname: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    createAt: {type: Date, default: Date.now}
+});
+
+var User = mongoose.model('user', userSchema);
+
 // view setting
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+app.use(session({secret: 'MySecret'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+var LocalStrategy = require('passport-local').Strategy;
+passport.use('local-login',
+    new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    function (req, email, password, done) {
+        User.findOne({'email': email}, function (err, user) {
+            if (err)
+                return done(err);
+
+            if (!user) {
+                req.flash("email", req.body.email);
+                return done(null, false, req.flash('loginError', 'No user found.'));
+            }
+
+            if (user.password != password) {
+                req.flash("email", req.body.email);
+                return done(null, false, req.flash('loginError', 'password does not match.'));
+            }
+
+            return done(null, user);
+        });
+    })
+);
 
 // set middleware
 app.use(express.static(path.join(__dirname, '/public')));
 
-// set routes
-// reference: mongoosejs.com/docs/api.html#model_Model.find
-app.get('/posts', function (req, res) {
-    Post.find({}, function (err, posts) {
+// set home routes
+app.get('/', function (req, res) {
+    res.redirect('/posts');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login/login', {email: req.flash("email")[0], loginError: req.flash('loginEroor')});
+});
+
+app.post('/login',
+    function (req, res, next) {
+        req.flash("email");     // flush email data
+
+        if (req.body.email.length == 0 || req.body.password.length === 0) {
+            req.flush("email", req.body.email);
+            req.flush("loginError", "Please enter both email and password.");
+            res.redirect('/login');
+        } else {
+            next();
+        }
+    },
+    passport.authenticate('local-login', {
+        successRedirect: '/posts',
+        failureRedirect: '/login',
+        failureFlash: true
+    })
+);
+
+app.get('/login', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+// set user routes
+app.get('/users/new', function (req, res) {
+    res.render('users/new', {
+        formData: req.flash('formData')[0],
+        emailError: req.flash('emailError')[0],
+        nicknameError: req.flash('nicknameError')[0],
+        passwordError: req.flash('passwordError')[0],
+    });
+}); // new
+
+app.post('/users', checkUserRegValidation, function (req, res, next) {
+    User.create(req.body.user, function(err, user) {
+        if (err)
+            return res.json({success: false, message: err});
+
+        res.redirect('/login');
+    });
+}); // create
+
+app.get('/user/:id', function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err)
+            return res.json({success: false, message: err});
+
+        res.render("users/show", {user: user});
+    });
+}); // show
+
+app.get('/users/:id/edit', function (req, res) {
+    User.findById(req.params.id, function (err, user) {
         if (err)
             res.json({success: false, message: err});
 
-        res.json({success: true, data: posts});
+        res.render("users/edit", {
+            user: user,
+            formData: req.flash("formData")[0],
+            emailError: req.flash("emailError")[0],
+            nicknameError: req.flash("nicknameError")[0],
+            passwordError: req.flash("passwordError")[0]
+        });
+    });
+}); // edit
+
+app.put('/users/:id', checkUserRegValidation, function (req, res) {
+    User.findById(req.params.id, req.body.user, function (err, user) {
+        if (err)
+            return res.json({success: false, message: err});
+
+        if (req.body.user.password == user.password) {
+            if (req.body.user.newPassword) {
+                req.body.user.password = req.body.user.newpassword;
+            } else {
+                delete req.body.user.password;
+            }
+
+            User.findByIdAndUpdate(req.params.id, req.body.user, function (err, user) {
+                if (err)
+                    return res.json({success: false, message: err});
+
+                res.render('/users/' + req.params.id);
+            });
+        } else {
+            req.flash("formData", req.body.user);
+            req.flash("passwordError", "- Invalid password");
+
+            req.redirect('/users/' + req.params.id + '/edit');
+        }
+    });
+}); // update
+
+function chackUserRegValidation(req, res, next) {
+    var inValid = true;
+
+    async.waterfall(
+        [function (callback) {
+            User.findOne({email: req.body.user.email, __id: {$ne: mongoose.Types.Object(req.params.id)}},
+                function (err, user) {
+                    if (user) {
+                        inValid = false;
+                        req.flash("emailError", "- This email is already registered.");
+                    }
+
+                    callback(null, isValid);
+                }
+            );
+        },
+        function (isValid, callback) {
+            User.findOne({nickname: req.body.user.nickname, _id: {$ne: mongoose.Types.Object(req.params.id)}},
+                function (err, user) {
+                    if (user) {
+                        isValid = false;
+                        req.flash("nicknameError", "- This nickname is already registered.");
+                    }
+
+                    callback(null, isValid);
+                }
+            );
+        }],
+        function (err, isValid) {
+            if (err)
+                return res.json({success: false, message: err});
+
+            if (isValid) {
+                return next();
+            } else {
+                req.flash("formData", req.body.user);
+                res.render("back");
+            }
+        }
+    );
+}
+
+// set post routes
+// reference: mongoosejs.com/docs/api.html#model_Model.find
+app.get('/posts', function (req, res) {
+    Post.find({}).sort('-createdAt').exec(function (err, posts) {
+        if (err)
+            res.json({success: false, message: err});
+
+        res.render("posts/index", {data: posts, user: req.user});
     });
 }); // index
+
+app.get('/posts/new', function (req, res) {
+    res.render('posts/new');
+}); // new
 
 app.post('/posts', function (req, res) {
     Post.create(req.body.post, function (err, post) {
         if (err)
             return res.json({success: false, message: err});
 
-        res.json({success: true, data: post});
+        res.redirect('/posts');
     });
 }); // create
 
@@ -65,9 +272,18 @@ app.get('/posts/:id', function (req, res) {
         if (err)
             return res.json({success: false, message: err});
 
-        res.json({success: true, data: post});
+        res.render("posts/show", {data: post});
     });
 }); // show
+
+app.get('/posts/:id/edit', function (req, res) {
+    Post.findById(req.params.id, function (err, post) {
+        if (err)
+            return res.json({success: false, message: err});
+
+        res.render("posts/edit", {data: post});
+    });
+});
 
 app.put('/posts/:id', function (req, res) {
     req.body.post.updatedAt = Date.now();
@@ -85,7 +301,7 @@ app.delete('/posts/:id', function (req, res) {
         if (err)
             return res.json({success: false, message: err});
 
-        res.json({success: true, message: post._id + " deleted"});
+        res.redirect('/posts');
     });
 }); // destroy
 
