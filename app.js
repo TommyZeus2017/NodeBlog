454b53dca4eff3b1c3fb933e198a46267db9f0c3
Tommy2017 <sacredzeus@outlook.com>
2017-03-21 10:06:10 +0800

    RESTful API - back end of posts without counter

--- app.js
+++ app.js
@@ -33,26 +33,6 @@ var postSchema = mongoose.Schema({
 
 var Post = mongoose.model('post', postSchema);
 
-var dataSchema = mongoose.Schema({
-    name: String,
-    count: Number
-});
-
-var Data = mongoose.model('data', dataSchema);
-
-Data.findOne({name: "myData"}, function (err, data) {
-    if (err)
-        return console.log("Data ERROR: ", err);
-    if (!data) {
-        Data.create({name: "myData", count: 0}, function (err, data) {
-            if (err)
-                return console.log("Data ERROR: ", err);
-
-            console.log("Counter initialized :", data);
-        });
-    }
-});
-
 // view setting
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
@@ -108,71 +88,6 @@ app.delete('/posts/:id', function (req, res) {
         res.json({success: true, message: post._id + " deleted"});
     });
 }); // destroy
-
-app.get('/', function (req, res) {
-    Data.findOne({name : "myData"}, function (err, data) {
-        if (err)
-            console.log("Data ERROR: ", err);
-
-        data.count++;
-
-        Data.save(function (err) {
-            if (err)
-                console.log("Data ERROR", err);
-
-            res.render('my_first_ejs', data);
-        });
-    });
-});
-
-app.get('/reset', function (req, res) {
-    setCounter(res, 0);
-});
-
-app.get('/set', function (req, res) {
-    if (req.query.count)
-        setCounter(res, req.query.count);
-    else
-        getCounter(res);
-});
-
-app.get('/set/:number', function (req, res) {
-    if (req.params.number)
-        setCounter(res, req.params.number);
-    else
-        getCounter(res);
-});
-
-function getCounter(res)
-{
-    console.log("getCounter");
-
-    Data.findOne({name: "myData"}, function (err, data) {
-        if (err)
-            return console.log("Data ERROR:", err);
-
-        res.render('my_first_ejs', data);
-    });
-}
-
-function setCounter(res, number)
-{
-    console.log("setCounter");
-
-    Data.findOne({name: "myData"}, function (err, data) {
-        if (err)
-            return console.log("Data ERROR:", err);
-
-        data.count = number;
-
-        data.save(function (err) {
-            if (err)
-                return console.log("Data ERROR:", err);
-
-            res.render('my_first_ejs', data);
-        });
-    });
-}
 
 // start server
 app.listen(3000, function() {
